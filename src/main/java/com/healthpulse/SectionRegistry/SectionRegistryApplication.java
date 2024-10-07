af7ad54222e2183f5e60ee868ac663d9bc3bf363
MD. SAIDUR RAHMAN SAGOR <saidurrahman@iut-dhaka.edu>
2024-10-07 07:07:07 +0600

    Add files via upload

---
+++ src/main/java/com/healthpulse/SectionRegistry/SectionRegistryApplication.java
@@ -0,0 +1,17 @@
+package com.healthpulse.SectionRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SectionRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectionRegistryApplication.class, args);
+		
+		System.out.println("Eureka Server is up and running...");
+	}
+
+}
